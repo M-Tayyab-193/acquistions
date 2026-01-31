ab6cfe57fab4907e6dbeed949f306c3c27a65fa6
MTayyab193 <tayyabprince193@gmail.com>
2026-01-31 22:07:10 +0500

    Implement Logging and Middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
+import logger from '#config/logger.js';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.status(200).send('Hello, from Acquisitions!'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
+app.get('/', (req, res) => {
+  logger.info('Root endpoint accessed');
+  res.status(200).send('Hello, from Acquisitions!');
+});
 
 export default app;
